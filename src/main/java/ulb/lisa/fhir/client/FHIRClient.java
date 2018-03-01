2c7e7093c11e25354104550cc033dc8845aed098
Adrien Foucart <adfoucart@gmail.com>
2018-03-01 11:37:04 +0100

    Interface and skeleton functions

---
+++ src/main/java/ulb/lisa/fhir/client/FHIRClient.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ulb.lisa.fhir.client;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import ca.uhn.fhir.rest.gclient.IQuery;
+import java.util.ArrayList;
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.Patient;
+
+/**
+ *
+ * @author Adrien Foucart
+ */
+public class FHIRClient {
+    public FhirContext ctx;
+    public IGenericClient client;
+    public String serverBase;
+        
+    public FHIRClient(){
+        ctx = FhirContext.forDstu3();
+        serverBase = "http://fhirtest.uhn.ca/baseDstu3";
+        client = ctx.newRestfulGenericClient(serverBase);
+    }
+    
+    public ArrayList<Patient> searchPatientsByName(String name){
+        ArrayList<Patient> searchResponse = new ArrayList();
+        
+        //IQuery<Bundle> query = ...;
+        //Bundle response = query.execute();
+        
+        //response.getEntry().stream().forEach((e) -> {
+        //    searchResponse.add( (Patient) e.getResource() );
+        //});
+        
+        return searchResponse;
+    }
+    
+    public Patient getPatientById(String id){  
+        Patient patient = new Patient();
+        //Patient patient = client.read()...
+        
+        return patient;
+    }
+    
+}
+
